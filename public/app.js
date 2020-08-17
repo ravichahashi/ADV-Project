ea97cc09e70097bbefed632c8a0c3b928b81c2d3
ravihashi <ravicha_hashimoto@cmu.ac.th>
2020-08-17 23:05:49 +0700

    test query user list

--- public/app.js
+++ public/app.js
@@ -1,56 +1,83 @@
 // const admin = require('firebase-admin');
+let userForm = document.querySelector(".userForm");
+userForm.addEventListener("submit", addUser);
 
 function Estimation(name, type, result, comment) {
-  this.estiName = name;
-  this.estiType = type;
-  this.estiResult = result;
-  this.estiComment = comment;
+  this.name = name;
+  this.type = type;
+  this.result = result;
+  this.comment = comment;
   return Object.assign({}, this);
 }
 
 function Child(name, age, score, nursID, userID) {
-  this.childName = name;
-  this.childAge = age;
-  this.childScore = score;
+  this.name = name;
+  this.age = age;
+  this.score = score;
   this.nursID = nursID;
   this.userID = userID;
+  return Object.assign({}, this);
+}
+
+function User(name, email, password, citizenID, phone, childID, nursID) {
+  this.name = name;
+  this.email = email;
+  this.password = password;
+  this.citizenID = citizenID;
+  this.phone = phone;
+  this.childID = childID;
+  this.nursID = nursID;
   return Object.assign({}, this);
 }
 
 document.addEventListener("DOMContentLoaded", (event) => {
   const app = firebase.app();
   const db = firebase.firestore();
-  const myPost = db
-    .collection("Children")
-    .doc("prototypeID")
-    .collection("Estimation")
-    .doc("estiID");
-  myPost.onSnapshot((doc) => {
-    const data = doc.data();
-    // document.write(data.title + `<br>`)
-    // document.write(data.createdAt + `<br>`)
-    console.log(data);
-    document.querySelector("#title").innerHTML = data.estiName;
+  const userList = db.collection("User");
+  fuserList = document.querySelector(".userList");
+  fuserList = ``
+  userList.onSnapshot((users) => {
+    users.forEach((user) => {
+      const data = user.data();
+      console.log(data)
+      // fuserList.innerHTML += `<p>${data.name}</p>`;
+    });
   });
 });
 
-function updatePost(e) {
-  let doc = document.querySelector("#doc").value;
-  console.log(doc);
-  let esdata = new Estimation("dd", 5, 1, "ddsa");
-  let esdata2 = new Estimation("ee", 6, 2, "dfwfdsa");
-  let data = new Child("ff", 12, 20, "1", "2");
-  console.log(data);
+function addUser(e) {
+  e.preventDefault();
   const db = firebase.firestore();
-  const newChild = db.collection("Children").doc(doc);
-  newChild.set(data);
-  const esti = newChild.collection("Estimation")
-  esti.doc(e.target.value).set(esdata);
-  esti.doc("ddg").set(esdata2);
-  esti.get().then((datas) => {
-    datas.forEach((data) => {
-      console.log(data.id, "=>", data.data());
-    });
-  });
-  // let score = results.reduce((a, b) => a + b, 0);
+
+  const name = userForm.querySelector(".name").value;
+  const email = userForm.querySelector(".email").value;
+  const password = userForm.querySelector(".password").value;
+  const citizenID = userForm.querySelector(".citizenID").value;
+  const phone = userForm.querySelector(".phone").value;
+  const userData = new User(name, email, password, citizenID, phone, [], []);
+  console.log(userData);
+
+  const newUser = db.collection("User").doc();
+  newUser.set(userData);
 }
+
+// function updatePost(e) {
+//   let doc = document.querySelector("#doc").value;
+//   console.log(doc);
+//   let esdata = new Estimation("dd", 5, 1, "ddsa");
+//   let esdata2 = new Estimation("ee", 6, 2, "dfwfdsa");
+//   let data = new Child("ff", 12, 20, "1", "2");
+//   console.log(data);
+//   const db = firebase.firestore();
+//   const newChild = db.collection("Children").doc(doc);
+//   newChild.set(data);
+//   const esti = newChild.collection("Estimation");
+//   esti.doc(e.target.value).set(esdata);
+//   esti.doc("ddg").set(esdata2);
+//   esti.get().then((datas) => {
+//     datas.forEach((data) => {
+//       console.log(data.id, "=>", data.data());
+//     });
+//   });
+//   // let score = results.reduce((a, b) => a + b, 0);
+// }
