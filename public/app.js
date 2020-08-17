d7ad7bfa932c7a662f0a5027a1f3d878e6ebfa1c
ravihashi <ravicha_hashimoto@cmu.ac.th>
2020-08-18 02:53:40 +0700

    kk

--- public/app.js
+++ public/app.js
@@ -3,109 +3,20 @@ const fuserList = document.querySelector(".userList");
 const userForm = document.querySelector(".userForm");
 userForm.addEventListener("submit", addUser);
 
+function Estimation(name, type, result, comment) {
+  this.name = name;
+  this.type = type;
+  this.result = result;
+  this.comment = comment;
+  return Object.assign({}, this);
+}
+
 function Child(name, age, score, nursID, userID) {
-  this.childName = name;
-  this.childAge = age;
-  this.childScore = score;
+  this.name = name;
+  this.age = age;
+  this.score = score;
   this.nursID = nursID;
   this.userID = userID;
-  return Object.assign({}, this);
-}
-
-function Estimation(name, age, result, comment, nursID) {
-  this.estiName = name;
-  this.estiAge = age;
-  this.estiResult = result;
-  this.estiComment = comment;
-  this.nursID = nursID;
-  return Object.assign({}, this);
-}
-
-function Nursery(name, password, pagedata, address, childID) {
-  this.nursName = name;
-  this.nursPass = password;
-  this.nursPagedata = pagedata;
-  this.nursAddress = address;
-  this.childID = childID;
-  return Object.assign({}, this);
-}
-
-function Pagedata(pic, header, description){
-  this.pagePic = pic;
-  this.pageHead = header;
-  this.pageDes = description;
-  return Object.assign({},this);
-}
-
-function ChatBox(userID, nursID){
-  this.userID = userID;
-  this.nursID = nursID;
-  return Object.assign({}, this);
-}
-
-function Chat(text, time, senderID){
-  this.chatText = text;
-  this.chatTime = time;
-  this.senderID = senderID;
-  return Object.assign({}, this);
-}
-
-function Request(status, fieldname, userID, nursID, childID){
-  this.reqStatus = status;
-  this.reqFname = fieldname;
-  this.userID = userID;
-  this.nursID = nursID;
-  this.childID - childID;
-  return Object.assign({}, this);
-}
-
-function QuestionGM(age, video, description, tool){
-  this.questGM_age = age;
-  this.questGM_video = video;
-  this.questGM_des = description;
-  this.questGM_tool = tool;
-  return Object.assign({}, this);
-}
-
-function QuestionFM(age, video, description, tool){
-  this.questFM_age = age;
-  this.questFM_video = video;
-  this.questFM_des = description;
-  this.questFM_tool = tool;
-  return Object.assign({}, this);
-}
-
-function QuestionRL(age, video, description, tool){
-  this.questRL_age = age;
-  this.questRL_video = video;
-  this.questRL_des = description;
-  this.questRL_tool = tool;
-  return Object.assign({}, this);
-}
-
-function QuestionEL(age, video, description, tool){
-  this.questEL_age = age;
-  this.questEL_video = video;
-  this.questEL_des = description;
-  this.questEL_tool = tool;
-  return Object.assign({}, this);
-}
-
-function QuestionPS(age, video, description, tool){
-  this.questPS_age = age;
-  this.questPS_video = video;
-  this.questPS_des = description;
-  this.questPS_tool = tool;
-  return Object.assign({}, this);
-}
-
-function NurseryUser(name, password, priority, email, phone, nursID){
-  this.nUserName = name;
-  this.nUserPass = password;
-  this.nUserPrio = priority;
-  this.nUserEmail = email;
-  this.nUserPhone = phone;
-  this.nursID = nursID;
   return Object.assign({}, this);
 }
 
@@ -125,7 +36,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
   const db = firebase.firestore();
   const userList = db.collection("User");
   userList.onSnapshot((users) => {
-    fuserList.innerHTML = ``
+    fuserList.innerHTML = ``;
     users.forEach((user) => {
       const data = user.data();
       fuserList.innerHTML += `<p>${data.name}</p>`;
@@ -133,13 +44,8 @@ document.addEventListener("DOMContentLoaded", (event) => {
   });
 });
 
-function updatePost(e) {
-  let doc = document.querySelector("#doc").value;
-  console.log(doc);
-  let esdata = new Estimation("dd", 5, 1, "ddsa", 0);
-  let esdata2 = new Estimation("ee", 6, 2, "dfwfdsa", 5);
-  let data = new Child("ff", 12, 20, "1", "2");
-  console.log(data);
+function addUser(e) {
+  e.preventDefault();
   const db = firebase.firestore();
   const name = userForm.querySelector(".name").value;
   const email = userForm.querySelector(".email").value;
