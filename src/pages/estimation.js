a3936ac58172a136d3be3389a0b178470c3d65a5
Rakpong <rakpongthorhun16648@gmail.com>
2020-09-18 19:58:29 +0700

    Update estimation.js

--- src/pages/estimation.js
+++ src/pages/estimation.js
@@ -1,9 +1,16 @@
-import React, {useEfftect, useState} from "react";
+import React, { useEffect, useState } from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
 
 const Estimation = () => {
-    return(
-        <a>Hello</a>
+    return (
+        <div className="col-md-10 col-lg-ุ6 offset-lg-1 wow bounceInUp" data-wow-delay="0.1s" data-wow-duration="1.4s">
+            <div className="box">
+                <div className="icon"><i className="ion-ios-paper-outline" style={{ color: '#3fcdc7' }} /></div>
+                <h4 className="title"><a href>ลงทะเบียน Nursery</a></h4>
+                <p className="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+            </div>
+        </div>
     )
 };
 
-export default Estimation ;
+export default Estimation;
