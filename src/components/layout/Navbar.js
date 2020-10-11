9a7f2caa6710f2b11e7ce08c2eb7e5040af61f96
Faikavi <35361400+Faikavi@users.noreply.github.com>
2020-10-12 03:29:05 +0700

    modify assResults

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -256,7 +256,7 @@ class Nevbar extends Component {
         <div className="logo float-left">
           <a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
           <a href="/" className="scrollto">
-            <img src="./sunerry2.png" alt className="img-fluid" />
+            <img src="./sunerry1.png" alt className="img-fluid" />
           </a>
         </div>
         <nav className="main-nav float-right d-none d-lg-block">
