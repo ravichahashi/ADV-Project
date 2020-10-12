d7ca910c21372938c60560fb1cbd5f26e8ac00c0
Rakpong <rakpongthorhun16648@gmail.com>
2020-10-13 00:42:47 +0700

    landing update

--- src/pages/Parent/landing.js
+++ src/pages/Parent/landing.js
@@ -1,101 +1,113 @@
 import React, { useEffect, useState } from "react";
 
 // Redux stuff
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
 // CSS
-import "../../components/NewBiz/lib/bootstrap/css/bootstrap.min.css"
-import "../../components/NewBiz/lib/font-awesome/css/font-awesome.min.css"
-import "../../components/NewBiz/lib/animate/animate.min.css"
-import "../../components/NewBiz/lib/ionicons/css/ionicons.min.css"
-import "../../components/NewBiz/lib/owlcarousel/assets/owl.carousel.min.css"
-import "../../components/NewBiz/lib/lightbox/css/lightbox.min.css"
-import "../../components/NewBiz/css/style.css"
+import "../../components/NewBiz/lib/bootstrap/css/bootstrap.min.css";
+import "../../components/NewBiz/lib/font-awesome/css/font-awesome.min.css";
+import "../../components/NewBiz/lib/animate/animate.min.css";
+import "../../components/NewBiz/lib/ionicons/css/ionicons.min.css";
+import "../../components/NewBiz/lib/owlcarousel/assets/owl.carousel.min.css";
+import "../../components/NewBiz/lib/lightbox/css/lightbox.min.css";
+import "../../components/NewBiz/css/style.css";
 
 const Index = () => {
-  const [Name, setName] = useState("เด็กชายสุริยา เตชะบุญ");
+  const [Child, setChild] = useState([]);
+
+  let item = [];
+  for (let key in Child) {
+    console.log(Child.length);
+    item.push(
+      <div
+        className="col-md-6 col-lg-5 offset-lg-1 wow bounceInUp"
+        data-wow-duration="1.4s"
+        align="center"
+      >
+        <div className="box">
+          <table>
+            <tr>
+              <td>
+                <div className="img-over">
+                  <a href="/addchild">
+                    <img src="./suriya.png" />
+                  </a>
+                </div>
+              </td>
+              <td>
+                <div className="row">
+                  <h4 className="title">
+                    <a>&nbsp;&nbsp;{Child[key].name}</a>
+                  </h4>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  <div className="img-overr">
+                    <a href="/editChild">
+                      <img src="./pencil.png" />
+                    </a>
+                  </div>
+                </div>
+                <p className="description">เพศ :{Child[key].sex}</p>
+                <p className="description">
+                  วันเกิด :{Child[key].birthDate.date} /{" "}
+                  {Child[key].birthDate.month} / {Child[key].birthDate.year}
+                </p>
+                <p className="description">น้ำหนัก :{Child[key].weigth}</p>
+                <p className="description">ส่วนสูง :{Child[key].height}</p>
+                <br></br>
+                <section id="more">
+                  <div class="form">
+                    <div align="center">
+                      {/* <a href="/assResults"> */}
+                      <a href={`/assResults?${Child[key].name}`}>
+                        <button type="submit">เพิ่มเติม</button>
+                      </a>
+                    </div>
+                  </div>
+                </section>
+              </td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div>
       <main id="main">
         <section id="your-fam">
           <div className="container" align-text="center">
-              <header className="section-header">
-                <h3><a href="/childmanager">ครอบครัวของคุณ</a></h3>
-                <section id="more">
+            <header className="section-header">
+              <h3>
+                <a href="/childmanager">ครอบครัวของคุณ</a>
+              </h3>
+              <section id="more">
                 <div class="form">
-                  <div align="center"><button type="submit" title="More"><a href="/childmanager">เพิ่มเติม</a></button></div>
+                  <div align="center">
+                    <button type="submit" title="More">
+                      <a href="/childmanager">เพิ่มเติม</a>
+                    </button>
+                  </div>
                 </div>
               </section>
               <p></p>
-              </header>
-            <div className="row row-eq-height justify-content-center">
+            </header>
+
+            {item}
+
+            {/* <div className="row row-eq-height justify-content-center">
               <div className="col-lg-3.5 mb-4">
                 <div className="card wow bounceInUp">
-                  {/* <i className="fa fa-diamond" /> */}
                   <div className="img-size"><img src="./suriya.png" /></div>
                   <div className="card-body">
                     <h5 className="card-title"><a href="/assResults?{Name}">{Name}</a></h5>
                     <p className="card-text">4 ขวบ</p>
-                    {/* <a href="#" className="readmore">Read more </a> */}
                   </div>
                 </div>
               </div>
-            </div>
+            </div> */}
           </div>
         </section>
-        {/* <section id="testimonials" className="section-bg">
-          <div className="container">
-            <header className="section-header">
-              <h3>Testimonials</h3>
-            </header>
-            <div className="row justify-content-center">
-              <div className="col-lg-8">
-                <div className="owl-carousel testimonials-carousel wow fadeInUp">
-                  <div className="testimonial-item">
-                    <img src="./suriya.png" className="testimonial-img" alt />
-                    <h3>Saul Goodman</h3>
-                    <h4>Ceo &amp; Founder</h4>
-                    <p>
-                      Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
-                    </p>
-                  </div>
-                  <div className="testimonial-item">
-                    <img src="img/testimonial-2.jpg" className="testimonial-img" alt /> 
-                    <h3>Sara Wilsson</h3>
-                    <h4>Designer</h4>
-                    <p>
-                      Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.
-                    </p>
-                  </div>
-                  <div className="testimonial-item">
-                    <img src="img/testimonial-3.jpg" className="testimonial-img" alt />
-                    <h3>Jena Karlis</h3>
-                    <h4>Store Owner</h4>
-                    <p>
-                      Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.
-                   </p>
-                  </div>
-                  <div className="testimonial-item">
-                    <img src="img/testimonial-4.jpg" className="testimonial-img" alt />
-                    <h3>Matt Brandon</h3>
-                    <h4>Freelancer</h4>
-                    <p>
-                      Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.
-                    </p>
-                  </div>
-                  <div className="testimonial-item">
-                    <img src="img/testimonial-5.jpg" className="testimonial-img" alt />
-                    <h3>John Larson</h3>
-                    <h4>Entrepreneur</h4>
-                    <p>
-                      Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
-                   </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>#testimonials */}
 
         <section id="why-us" className="wow fadeIn">
           <div className="container">
@@ -111,7 +123,9 @@ const Index = () => {
                     <h5 className="card-title">Name Nursery love love</h5>
                     <p className="card-text">Address 123</p>
                     <p className="card-text">Phone 088-8888888</p>
-                    <a href="#" className="readmore">Read more </a>
+                    <a href="#" className="readmore">
+                      Read more{" "}
+                    </a>
                   </div>
                 </div>
               </div>
@@ -122,7 +136,9 @@ const Index = () => {
                     <h5 className="card-title">Name Nursery love love</h5>
                     <p className="card-text">Address 123</p>
                     <p className="card-text">Phone 088-8888888</p>
-                    <a href="#" className="readmore">Read more </a>
+                    <a href="#" className="readmore">
+                      Read more{" "}
+                    </a>
                   </div>
                 </div>
               </div>
@@ -133,7 +149,9 @@ const Index = () => {
                     <h5 className="card-title">Name Nursery love love</h5>
                     <p className="card-text">Address 123</p>
                     <p className="card-text">Phone 088-8888888</p>
-                    <a href="#" className="readmore">Read more </a>
+                    <a href="#" className="readmore">
+                      Read more{" "}
+                    </a>
                   </div>
                 </div>
               </div>
@@ -144,7 +162,9 @@ const Index = () => {
                     <h5 className="card-title">Name Nursery love love</h5>
                     <p className="card-text">Address 123</p>
                     <p className="card-text">Phone 088-8888888</p>
-                    <a href="#" className="readmore">Read more </a>
+                    <a href="#" className="readmore">
+                      Read more{" "}
+                    </a>
                   </div>
                 </div>
               </div>
@@ -155,7 +175,9 @@ const Index = () => {
                     <h5 className="card-title">Name Nursery love love</h5>
                     <p className="card-text">Address 123</p>
                     <p className="card-text">Phone 088-8888888</p>
-                    <a href="#" className="readmore">Read more </a>
+                    <a href="#" className="readmore">
+                      Read more{" "}
+                    </a>
                   </div>
                 </div>
               </div>
@@ -166,7 +188,9 @@ const Index = () => {
                     <h5 className="card-title">Name Nursery love love</h5>
                     <p className="card-text">Address 123</p>
                     <p className="card-text">Phone 088-8888888</p>
-                    <a href="#" className="readmore">Read more </a>
+                    <a href="#" className="readmore">
+                      Read more{" "}
+                    </a>
                   </div>
                 </div>
               </div>
@@ -177,7 +201,9 @@ const Index = () => {
                     <h5 className="card-title">Name Nursery love love</h5>
                     <p className="card-text">Address 123</p>
                     <p className="card-text">Phone 088-8888888</p>
-                    <a href="#" className="readmore">Read more </a>
+                    <a href="#" className="readmore">
+                      Read more{" "}
+                    </a>
                   </div>
                 </div>
               </div>
@@ -188,7 +214,9 @@ const Index = () => {
                     <h5 className="card-title">Name Nursery love love</h5>
                     <p className="card-text">Address 123</p>
                     <p className="card-text">Phone 088-8888888</p>
-                    <a href="#" className="readmore">Read more </a>
+                    <a href="#" className="readmore">
+                      Read more{" "}
+                    </a>
                   </div>
                 </div>
               </div>
@@ -199,7 +227,9 @@ const Index = () => {
                     <h5 className="card-title">Name Nursery love love</h5>
                     <p className="card-text">Address 123</p>
                     <p className="card-text">Phone 088-8888888</p>
-                    <a href="#" className="readmore">Read more </a>
+                    <a href="#" className="readmore">
+                      Read more{" "}
+                    </a>
                   </div>
                 </div>
               </div>
@@ -207,9 +237,7 @@ const Index = () => {
           </div>
         </section>
       </main>
-
     </div>
-
   );
 };
 
